367fc40bc4d09593b4f76862294c292dd20fb869
unknown <kritsadamoungkhao@gmail.com>
2020-04-23 19:41:16 +0700

    Day 19 Autocomplete

---
+++ public/day19/scripts.js
@@ -0,0 +1,48 @@
+(() => {
+  const carBrands = [
+    'BMW',
+    'Maserati',
+    'Mercedes Benz',
+    'Ferrari',
+    'Toyota',
+    'Honda',
+    'Hyundai',
+  ];
+  const searchElem = document.querySelector('.search');
+
+  function clearResults() {
+    const ulElem = document.body.querySelector('.results');
+    if (ulElem) {
+      document.body.removeChild(ulElem);
+    }
+  }
+
+  function selectCarBrand(event) {
+    searchElem.value = event.target.innerText;
+    clearResults();
+  }
+
+  function onInput(event) {
+    clearResults()
+    const inputText = event.target.value.toLowerCase();
+    const matchedCarBrands = carBrands.filter((carBrands) =>
+      carBrands.toLowerCase().startsWith(inputText)
+    );
+    const ulElem = document.createElement('ul');
+    ulElem.classList.add('results');
+
+    matchedCarBrands.forEach((carBrand) => {
+      const liElem = document.createElement('li');
+      liElem.innerText = carBrand;
+      liElem.onclick = selectCarBrand;
+      ulElem.appendChild(liElem);
+    });
+    document.body.appendChild(ulElem);
+  }
+  function run() {
+    searchElem.addEventListener('input', onInput);
+    document.addEventListener('click', clearResults)
+  }
+
+  run();
+})();
